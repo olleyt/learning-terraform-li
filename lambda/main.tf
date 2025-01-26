580c62209a7fe685e52f62081d9f6bc37b88f1f2
Olga Timofeeva <32430092+olleyt@users.noreply.github.com>
2025-01-26 06:34:05 +0000

    fix providers and add eventbridge rule

--- lambda/main.tf
+++ lambda/main.tf
@@ -8,3 +8,24 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 
   eventbridge = true
 }
+
+# EventBridge Rule
+resource "aws_cloudwatch_event_rule" "s3_event" {
+  name        = "capture-s3-events"
+  description = "Capture S3 events for specific folder"
+
+  event_pattern = jsonencode({
+    source      = ["aws.s3"]
+    detail-type = ["Object Created"]
+    detail = {
+      bucket = {
+        name = [aws_s3_bucket.tf-lambda-s3-events-260125-bucket.id]
+      }
+      object = {
+        key = [{
+          prefix = "input-folder/"  # Change to your desired folder path
+        }]
+      }
+    }
+  })
+}
